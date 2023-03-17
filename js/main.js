aef15f35215d8b90f62f68f18022ee96a1c17c80
juanituza <juanituza85@gmail.com>
2023-03-17 11:18:10 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -132,3 +132,31 @@ const eliminarEgreso = (id) => {
   cargarCabecero();
   cargarEgresos();
 };
+
+
+// let boton = document.getElementById("button");
+
+
+let agregarDato = () => {
+  let forma = document.forms['forma'];
+  let tipo = forma ['tipo'];
+  let descripcion = forma ['descripcion'];
+  let valor = forma ['valor'];
+
+  if (descripcion.value !== '' && valor.value !== '') {
+    if (tipo.value === 'ingreso' ) {
+      ingresos.push(new Ingreso(descripcion.value,+valor.value));
+      cargarCabecero();
+      cargarIngresos();
+    }else if(tipo.value === 'egreso'){
+      egresos.push(new Egreso(descripcion.value, +valor.value));
+      cargarCabecero();
+      cargarEgresos();
+     }
+  }
+
+
+}
+
+
+// boton.addEventListener("submit", () => agregarDato());
